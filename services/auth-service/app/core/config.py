5d2dd7250b9cf803bff079c7befd3488470d1bed
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-11 20:48:25 +0530

    Implement token service with JWT handling and add refresh token repository

--- services/auth-service/app/core/config.py
+++ services/auth-service/app/core/config.py
@@ -3,6 +3,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    JWT_SECRATE_KEY :str = 'D'
+    JWT_ALGORITHM :str = 'HS256'
+    REFRESH_TOKEN_EXPIRE_MINUTS : int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS : int = 7
 
     model_config = SettingsConfigDict(
         env_file=".env",
