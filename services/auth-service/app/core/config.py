45119566780fd5c791f407e82ed41d72a74d9c5c
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-12 13:42:35 +0530

    Add logging setup and fix access token expiration variable name

--- services/auth-service/app/core/config.py
+++ services/auth-service/app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRATE_KEY :str = 'D'
     JWT_ALGORITHM :str = 'HS256'
-    REFRESH_TOKEN_EXPIRE_MINUTS : int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTS : int = 30
     REFRESH_TOKEN_EXPIRE_DAYS : int = 7
 
     model_config = SettingsConfigDict(
--- services/auth-service/app/main.py
+++ services/auth-service/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api.router import api_router
+from app.core.logging import setup_logging
 
+setup_logging()
 
 app =  FastAPI(
     title="Auth Service"
