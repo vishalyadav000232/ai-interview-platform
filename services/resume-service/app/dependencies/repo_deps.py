d27f5f593f89143368335364a8bbac208656c5c8
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-25 09:17:27 +0530

    Add repository dependency functions for ResumeProfile, ResumeSkill, and ResumeEducation

--- services/resume-service/app/dependencies/repo_deps.py
+++ services/resume-service/app/dependencies/repo_deps.py
@@ -1,11 +1,38 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.database.session import get_db
 
-
 from app.repository.interface.resume import ResumeRepositoryInterface
+from app.repository.interface.resume_profile import ResumeProfileRepositoryInterface
+from app.repository.interface.resume_skill import ResumeSkillRepositoryInterface
+from app.repository.interface.resume_education import ResumeEducationRepositoryInterface
+
 from app.repository.resume import ResumeRepository
+from app.repository.resume_profile import ResumeProfileRepository
+from app.repository.resume_skills import ResumeSkillRepository
+from app.repository.resume_education import ResumeEducationRepository
+
+
 async def get_resume_repo(
-    db : AsyncSession = Depends(get_db)
-)-> ResumeRepositoryInterface:
-    return ResumeRepository(db=db)
+    db: AsyncSession = Depends(get_db),
+) -> ResumeRepositoryInterface:
+    return ResumeRepository(db)
+
+
+async def get_resume_profile_repository(
+    db: AsyncSession = Depends(get_db),
+) -> ResumeProfileRepositoryInterface:
+    return ResumeProfileRepository(db)
+
+
+async def get_resume_skill_repository(
+    db: AsyncSession = Depends(get_db),
+) -> ResumeSkillRepositoryInterface:
+    return ResumeSkillRepository(db)
+
+
+async def get_resume_education_repository(
+    db: AsyncSession = Depends(get_db),
+) -> ResumeEducationRepositoryInterface:
+    return ResumeEducationRepository(db)
