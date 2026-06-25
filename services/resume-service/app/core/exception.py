aa5f42d5abb6955f27f56f5174a00f1618054767
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-25 14:09:19 +0530

    Add custom exception handling for ResumeException and global exceptions

---
+++ services/resume-service/app/core/exception.py
@@ -0,0 +1,15 @@
+
+
+class ResumeException(Exception):
+
+    def __init__(
+        self,
+        message: str,
+        status_code: int,
+        error_code: str,
+    ):
+        self.message = message
+        self.status_code = status_code
+        self.error_code = error_code
+
+        super().__init__(message)
---
+++ services/resume-service/app/core/exception_builder.py
@@ -0,0 +1,43 @@
+import logging 
+from fastapi import Request , FastAPI
+
+from app.core.exception import ResumeException
+
+from fastapi.responses import JSONResponse
+
+logger = logging.getLogger(__name__)
+
+async def app_exception_handler(
+    request : Request,
+    exc : ResumeException
+):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "success":False,
+            "message":exc.message,
+            "error_code": exc.error_code
+        }
+    )
+   
+    
+async def global_exception_handler(
+    request : Request,
+    exc : Exception
+):
+    logger.exception(
+        "Unexpected server error"
+    )
+    return JSONResponse(
+        status_code=500,
+        content={
+            "success": False,
+            "message": "Internal server error",
+            "error_code": "INTERNAL_SERVER_ERROR"
+        }
+    )
+
+
+def register_exception_handlers(app: FastAPI):
+    app.add_exception_handler(ResumeException ,  app_exception_handler)
+    app.add_exception_handler(Exception, global_exception_handler)
