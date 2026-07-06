268c43e7bd8ae12f9a5f5176d3560d10383cbe98
VishaL Yadav <vishalyadav000232@gmail.com>
2026-07-06 10:37:58 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added main application structure with App component and basic styling.
    - Integrated SVG icons for various functionalities.
    - Configured TypeScript with appropriate settings for app and node environments.
    - Set up Vite as the build tool with React and Tailwind CSS plugins.
    - Included assets such as hero image and logos for React and Vite.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center">
+  this is app for ai-intervies platforms
+    </div>
+  )
+}
+
+export default App
